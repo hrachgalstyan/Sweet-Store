924052af7610cca673174e9f9a984e72df0c7545
hrachgalstyan <hrachgalstyann@gmail.com>
2020-06-08 00:53:24 +0400

    .

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -11,7 +11,7 @@ export default function Product({ image, title, id, price}) {
     </div>
     <div className="product-footer">
       <p className="product-title">{title}</p>
-      <p className="product-price">${price}</p>
+      <p className="product-price">{price}դր</p>
     </div>
   </article>
 }
